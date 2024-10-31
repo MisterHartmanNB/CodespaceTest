3068f47000652b05f47800c02c5e5ca2849666cb
John Hartman <john.hartman@newbremenschools.org>
2024-10-31 19:04:57 +0000

    p5 Code added

--- sketch.js
+++ sketch.js
@@ -4,4 +4,10 @@ function setup() {
 
 function draw() {
   background(220);
+  fill('blue');
+  rect(50,100,300,200);
+  fill('red');
+  circle(200,200,200);
+  fill('purple');
+  rect(0,200,400,50);
 }
